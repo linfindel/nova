8fc54adb79d149eebcdea99cb25ee887f9e920f9
Nadir Technologies <116459177+Nadir-Software@users.noreply.github.com>
2023-08-03 20:40:15 +0100

    Add files via upload

---
+++ nova.js
@@ -0,0 +1,95 @@
+const landingSection = document.getElementById("landing");
+const contentSection = document.getElementById("content");
+const titleSection = document.getElementById("title");
+const descriptionSection = document.getElementById("description");
+const articleSection = document.getElementById("article");
+const imageSection = document.getElementById("image");
+const searchBox = document.getElementById("search");
+const copyArticleButton = document.getElementById("copy-article");
+const openImageButton = document.getElementById("open-image");
+
+var title;
+var description;
+var article;
+var pageURL;
+
+function search() {
+    var searchTerm = searchBox.value;
+    searchTerm = searchTerm.replace(/(\?|\ba\b|\ban\b|\bwhat is\b|\bwhat are\b|\bwho is\b|\bwho was\b|\bwhere is\b|\bwhere was\b|\bthe\b)/gi, "").trim()
+
+    if (searchTerm != ""){
+        fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(searchTerm)}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            title = data.title;
+            description = data.description;
+            article = data.extract;
+            pageURL = data.content_urls.desktop.page;
+
+            if (data.originalimage && data.originalimage.source) {
+                var imageURL = data.originalimage.source;
+            }
+
+            titleSection.innerText = title;
+            descriptionSection.innerText = description;
+            articleSection.innerText = article;
+
+            if (imageURL) {
+                imageSection.style.backgroundImage = `url('${imageURL}')`;
+                imageSection.style.display = "flex";
+                openImageButton.style.display = "flex";
+            }
+
+            else {
+                imageSection.style.display = "none";
+                openImageButton.style.display = "none";
+            }
+
+            landingSection.style.transform = "translateY(100vh)";
+            contentSection.style.transform = "translateY(-40vh)";
+        })
+        .catch(error => {
+            document.getElementById("search-container").className = "search-box-error";
+
+            setTimeout(() => {
+                document.getElementById("search-container").className = "search-box";
+            }, 1000);
+        });
+    }
+
+    else {
+        landingSection.style.transform = "translateY(20vh)";
+        contentSection.style.transform = "translateY(100vh)";
+    }
+}
+
+function copy() {
+    try {
+        navigator.clipboard.writeText = articleSection.innerText;
+        copyArticleButton.classList.add("button-icon-success");
+
+        setTimeout(() => {
+            copyArticleButton.classList.remove("button-icon-success");
+        }, 1000);
+    }
+
+    catch {
+        copyArticleButton.classList.add("button-icon-error");
+
+        setTimeout(() => {
+            copyArticleButton.classList.remove("button-icon-error");
+        }, 1000);
+    }
+}
+
+function openInNew(content) {
+    if (content == "article") {
+        open(pageURL, "_blank");
+    }
+
+    else if (content == "image") {
+        open(imageURL, "_blank");
+    }
+}
